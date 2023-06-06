793055989746311c2f794a97a041bbd9ef20cded
André-git <andre.potthast.ap@gmail.com>
2023-06-06 14:28:32 +0200

    added webpack dev server

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,10 @@
-console.log("index.js works?");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("index.js works?")
+conole.log("index.js works?")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0EsNkIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9vZGluLXJlc3RhdXJhbnRwYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiaW5kZXguanMgd29ya3M/XCIpXG5jb25vbGUubG9nKFwiaW5kZXguanMgd29ya3M/XCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
