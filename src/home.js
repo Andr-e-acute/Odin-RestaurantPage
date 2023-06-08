666512893ca0b5137504e46483e7fcd023e75334
André-git <andre.potthast.ap@gmail.com>
2023-06-08 10:56:05 +0200

    contact and home finished for now

--- src/home.js
+++ src/home.js
@@ -1,4 +1,27 @@
 function createHome(){
-    console.log("home called this works?")
+    const home=document.createElement("div")
+    home.id=("home")
+
+    home.appendChild(createElement('p',"The perfect pasta every time."))
+    home.appendChild(createElement('p',"Welcome to Al Dente,"))
+    home.appendChild(createElement('p',"the best place to enjoy authentic Italian pasta."))
+    const img =document.createElement("img");
+    img.src="images/chef.jpg"
+    img.alt="Ai generate picture of italian cook"
+    home.appendChild(img)
+    home.appendChild(createElement('p',"Order online or visit us!"))
+    return home
 }
-export{createHome}
+
+function createElement(type,textContent){
+    const p =document.createElement(type)
+    p.textContent=textContent
+    return p
+}
+export{createHome, createElement}
+{/* <p>
+Welcome to Al Dente,<br />
+the best place to enjoy authentic Italian pasta
+</p>
+<img src="images/chef.jpg" alt="Ai generate pic of italina cook" />
+<p>Order online or visit us!</p> */}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import { createPage } from "./page-load";
 import { createHome } from "./home";
 import { createMenu } from "./menu";
 import { createContact } from "./contact";
+
 // createPage empty Page with header footer background
 createPage();
+const main =document.querySelector("main")
+
 
 // tab switching logic
 const tabs = document.querySelectorAll("nav button");
+// first time load set home-page active
+    // the right place would be page-load/? need to try it out later.....
+setActive(document.querySelector("#contact-button"))
+
 tabs.forEach((tab) => {
   tab.addEventListener("click", (e) => {
     setActive(e.target);
@@ -14,8 +21,11 @@ tabs.forEach((tab) => {
   });
 });
 function setActive(clickedButton) {
+console.log(clickedButton)
   clickedButton.classList.add("active");
+       
   chooseContent(clickedButton.dataset.target);
+
   tabs.forEach((tab) => {
     if (tab !== clickedButton) {
       tab.classList = "";
@@ -23,16 +33,18 @@ function setActive(clickedButton) {
   });
 }
 function chooseContent(choice) {
-    // reset and append content here oder for every file?
+    // reset and append content here oder for every file itself?
+     main.textContent="";
+
   switch (choice) {
     case "Home":
-      createHome();
+      main.appendChild(createHome());
       break;
     case "Menu":
       createMenu();
       break;
     case "Contact":
-      createContact();
+      main.appendChild(createContact());
       break;
     default:
       break;
